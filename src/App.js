2fbe3700d1ffc9bb7493863ae3d8b18650a6e317
Anthony Nguyen <anthonybnguyen56@gmail.com>
2024-10-11 15:59:11 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import NavBar from './components/Navbar';
+import Home from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+  <>
+    <NavBar/>
+    <Home />
+    <About />
+    <Services />
+    <Tours />
+    <Footer/>
+  </>
+  );
+}
+
+export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import logo from '../images/logo.svg'
+import {pageLinks} from '../data'
+import {socialLinks} from '../data'
+
+const Navbar = () => {
+  return (
+        <nav class="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <img src={logo} class="nav-logo" alt="backroads" />
+          <button type="button" class="nav-toggle" id="nav-toggle">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul class="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className='nav-link'></a>
+                {link.text}
+              </li>
+            )
+          })}
+        </ul>
+        <ul class="nav-icons">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a
+                  href={link.href}
+                  target="_blank"
+                  className="nav-icon"
+                >
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Navbar
